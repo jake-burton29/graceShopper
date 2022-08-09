c5c76538c990155dd5038285959e1349d58b7bdd
jake-burton29 <jake.burton29@gmail.com>
2022-08-09 15:24:39 -0600

    file structure as well as index.js in /api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,27 @@
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
+const usersRouter = require("./users");
+const productsRouter = require("./products");
+const ordersRouter = require("./orders");
+const product_ordersRouter = require("./product_orders");
+const categoriesRouter = require("./categories");
 
-apiRouter.get('/', (req, res, next) => {
+apiRouter.get("/", (req, res, next) => {
   res.send({
-    message: 'API is under construction!',
+    message: "API is under construction!",
   });
 });
 
-apiRouter.get('/health', (req, res, next) => {
+apiRouter.get("/health", (req, res, next) => {
   res.send({
     healthy: true,
   });
 });
 
 // place your routers here
+// apiRouter.use("/users", usersRouter);
+// apiRouter.use("/products", productsRouter);
+// apiRouter.use("/orders", ordersRouter);
+// apiRouter.use("/product_orders", product_ordersRouter);
+// apiRouter.use("/categories", categoriesRouter);
 
 module.exports = apiRouter;
