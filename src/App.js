68457cbb71510dd6e2861b4b02bc9eb3114b10ff
johnedvalson <califaxthecool@gmail.com>
2022-08-10 12:50:22 -0600

    fixed react dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
 import { getAPIHealth } from "./axios-services";
-import "../style/App.css";
+import "./style/App.css";
 import {
   Home,
   NavBar,
