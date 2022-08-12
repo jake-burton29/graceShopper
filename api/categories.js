3e8298e9707436bfc6bbb24ca19fe3eceed13b81
Comptonvz <comptonmain@hotmail.com>
2022-08-12 10:30:34 -0700

    worked on the categories api file

---
+++ api/categories.js
@@ -0,0 +1,68 @@
+const categoriesRouter = require("express").Router();
+const prisma = require("../db/prisma");
+
+// getCategories
+categoriesRouter.get("/", async (req, res, next) => {
+  try {
+    const getAllCategories = await prisma.categories.findMany();
+    res.send(getAllCategories);
+  } catch (error) {
+    next(error);
+  }
+});
+// getCategoriesId
+categoriesRouter.get("/:id", async (req, res, next) => {
+  try {
+    const categoryId = +req.params.id;
+    const getCategoryById = await prisma.categories.findUnique({
+      where: { id: categoryId },
+    });
+    res.send(getCategoryById);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /categories - admin
+categoriesRouter.post("/", async (req, res, next) => {
+  try {
+    const { name, description } = req.body;
+    const createCategory = await prisma.categories.create({
+      data: { name, description },
+    });
+    res.send(createCategory);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PATCH /categories/:categoryId - admin
+categoriesRouter.patch("/:id", async (req, res, next) => {
+  try {
+    const categoryId = +req.params.id;
+    const { name, description, id } = req.body; // id could be wrong will fix DONT FORGFET
+    const editCategory = await prisma.categories.update({
+      where: { id: categoryId },
+      data: { name, description, id },
+    });
+    res.send(editCategory);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//  DELETE /categories/:categoryId - admin
+categoriesRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const categoryId = +req.params.id;
+
+    const deleteCategory = await prisma.categories.delete({
+      where: { id: categoryId },
+    });
+    res.send(deleteCategory);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = categoriesRouter;
