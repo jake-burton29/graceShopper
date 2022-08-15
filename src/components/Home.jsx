1a1b91434dd9c6bc7d8f615306b7cc8b7cacc47c
1hazardousboi <sarinjthorn@gmail.com>
2022-08-15 12:58:47 -0600

    installed bootstrap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button variant="success">TEST BUTTON</Button>
+      <Card className="fixed-bottom">
+        <Card.Body>
+          <Card.Title>Some Product Title</Card.Title>
+          <Card.Text>Product description here</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
