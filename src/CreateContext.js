ca7ba33b13e7d8693608533fce6cc1a825176396
Comptonvz <comptonmain@hotmail.com>
2022-08-15 09:07:08 -0700

    createdContext

---
+++ src/CreateContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const AuthContext = createContext([]);
+const OrdersContext = createContext([]);
+const CategoriesContext = createContext([]);
+const ProductsContext = createContext([]);
+
+export { AuthContext, OrdersContext, CategoriesContext, ProductsContext };
