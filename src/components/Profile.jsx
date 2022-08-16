1d2d101c0456e23b692fc6bbd3846580544e0147
johnedvalson <califaxthecool@gmail.com>
2022-08-16 15:19:36 -0600

    started admin panel

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
+import { Button } from "react-bootstrap";
+import axios from "axios";
 
 export default function Profile() {
   const { user } = useAuth();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [image_url, setImage_url] = useState("");
+  const [inventory, setInventory] = useState("");
+  const [categoryId, setCategoryId] = useState("");
 
   return (
-    <div>
+    <div id="userInfo">
       {user ? (
         <div>
           <h3>Account Info:</h3>
@@ -36,6 +44,62 @@ export default function Profile() {
             </div>
           );
         })}
+      </div>
+
+      <div id="adminPanel">
+        <h3>ADMIN CONTROL PANEL</h3>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const { data: product } = await axios({
+              method: "post",
+              url: "/api/products/",
+              data: {
+                name,
+                price: +price,
+                description,
+                image_url,
+                inventory: +inventory,
+                categoryId: +categoryId,
+              },
+            });
+            console.log(product);
+          }}
+        >
+          <input
+            value={name}
+            placeholder="Product Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            value={price}
+            placeholder="Product Price"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <input
+            value={description}
+            placeholder="Product Description"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            value={inventory}
+            placeholder="Product Inventory"
+            onChange={(e) => setInventory(e.target.value)}
+          />
+          <input
+            value={categoryId}
+            placeholder="Product Category #"
+            onChange={(e) => setCategoryId(e.target.value)}
+          />
+          <input
+            value={image_url}
+            placeholder="Image URL"
+            onChange={(e) => setImage_url(e.target.value)}
+          />
+          <Button variant="success" type="submit">
+            Create New Product
+          </Button>
+        </form>
       </div>
     </div>
   );
