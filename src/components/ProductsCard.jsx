026d8cd2c9731bcb5ffeaad17cd9b6df8608a719
1hazardousboi <sarinjthorn@gmail.com>
2022-08-16 14:02:13 -0600

    single product work

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Breadcrumb } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function ProductsCard({ product }) {
+  const navigate = useNavigate();
+
   return (
     <div className="d-flex flex-nowrap">
       <Card>
-        <Card.Title>Product: {product.name}</Card.Title>
+        <Breadcrumb
+          onClick={() => {
+            navigate(`/products/${product.id}`);
+          }}
+        >
+          Product: {product.name}
+        </Breadcrumb>
         <Card.Img src={product.image_url} className="w-50 p-3" />
         <Card.Body>Price: {product.price}</Card.Body>
         <Card.Text>In Stock: {product.inventory}</Card.Text>
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 export default function SingleProduct() {
-  return <div>SingleProduct</div>;
+  return (
+    <div>
+      <Card>
+        <Card.Title>Product: {product.name}</Card.Title>
+        <Card.Img src={product.image_url} className="w-50 p-3" />
+        <Card.Body>Price: {product.price}</Card.Body>
+        <Card.Text>In Stock: {product.inventory}</Card.Text>
+        <Card.Text>Description: {product.description}</Card.Text>
+        <Button
+          onClick={() => {
+            //some func to add to cart
+          }}
+        >
+          Add to Cart!
+        </Button>
+      </Card>
+    </div>
+  );
 }
