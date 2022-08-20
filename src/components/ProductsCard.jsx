7e1133da903c48d4e1f43aaf35d0ae6309d629d3
johnedvalson <califaxthecool@gmail.com>
2022-08-20 12:36:37 -0600

    removed comment

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -12,54 +12,6 @@ export default function ProductsCard({ product }) {
   const navigate = useNavigate();
   const { cart, setCart } = useCart();
   const { user } = useAuth();
-  // async function addToCart() {
-  //   let productOrderIndex = -1;
-  //   if (user) {
-  //     productOrderIndex = cart.product_orders?.findIndex(
-  //       (product_order) => product_order.productId === product.id
-  //     );
-  //   }
-  //   if (cart.product_orders && productOrderIndex !== -1) {
-  //     if (user) {
-  //       await editProductOrder(
-  //         cart.product_orders[productOrderIndex].quantity + 1,
-  //         cart.product_orders[productOrderIndex].id
-  //       );
-  //       const cartCopy = { ...cart };
-  //       cartCopy.product_orders[productOrderIndex].quantity += 1;
-  //       setCart(cartCopy);
-  //     }
-  //     console.log("Adding another to cart!");
-  //   } else {
-  //     if (user) {
-  //       console.log("Creating a new product_order!");
-  //       const newProductOrder = await createProductOrder(
-  //         product.id,
-  //         cart.id,
-  //         1
-  //       );
-  //       if (cart.product_orders) {
-  //         setCart({
-  //           ...cart,
-  //           product_orders: [...cart.product_orders, newProductOrder],
-  //         });
-  //       } else {
-  //         setCart({ ...cart, product_orders: [newProductOrder] });
-  //       }
-  //     } else if (cart[product.id]) {
-  //       const cartCopy = { ...cart };
-  //       cartCopy[product.id] += 1;
-  //       setCart(cartCopy);
-  //       localStorage.setItem("guestCart", JSON.stringify(cart));
-  //     } else {
-  //       setCart({ ...cart, [product.id]: 1 });
-  //       localStorage.setItem(
-  //         "guestCart",
-  //         JSON.stringify({ ...cart, [product.id]: 1 })
-  //       );
-  //     }
-  //   }
-  // }
   async function addToCart() {
     let productOrderIndex = -1;
     productOrderIndex = cart.product_orders?.findIndex(
