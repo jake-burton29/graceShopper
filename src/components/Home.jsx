45f049480c83fee4a14509812fee11b3e46820e0
1hazardousboi <sarinjthorn@gmail.com>
2022-08-22 14:59:54 -0600

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import useProducts from "../hooks/useProducts";
 import useCategories from "../hooks/useCategories";
 import ProductsCard from "./ProductsCard";
-import { Button, Carousel } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { getProductsByCategoryId } from "../axios-services/products";
+import Carousel from "./Carousel";
+
+// importing from react-bootstrap-icons is possible!
 
 export default function Home() {
   const { products } = useProducts();
@@ -25,60 +28,17 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        {
-          // Break Carousel into separate component
-        }
-        <Carousel variant="dark">
-          <Carousel.Item>
-            <img
-              className="d-block mx-auto "
-              style={{ marginBottom: 140 }}
-              src="https://res.cloudinary.com/dgnfm1iyp/image/upload/v1660935299/R2S-8_1_22_badiyy.png"
-              alt="First slide"
-              height={400}
-            />
-            <Carousel.Caption className="bgC">
-              <h3>Godly Starter Streaming PC's </h3>
-              <p>For real.. for on sale for only 9,000$</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block mx-auto "
-              style={{ marginBottom: 140 }}
-              src="https://res.cloudinary.com/dgnfm1iyp/image/upload/v1660934931/6471870cv2d_wmoynh.jpg"
-              alt="Second slide"
-              height={400}
-            />
-            <Carousel.Caption className="bgC">
-              <h3>Why wont these images center??</h3>
-              <p>
-                We just need some bottom padding, greyed out sides, a caption,
-                and it'd look ok
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block mx-auto "
-              style={{ marginBottom: 140 }}
-              src="https://res.cloudinary.com/dgnfm1iyp/image/upload/v1660934718/PhoenixMain45-Edit-Edit_dlree6.jpg"
-              alt="Third slide"
-              height={400}
-            />
-            <Carousel.Caption className="bgC">
-              <h3>Custom Keyboards</h3>
-              <p>
-                We sell some of the most popular custom keybaords, shipping
-                world wide with 10% sale with the purchase of two items or more!
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-        <h3 className="category">Categories:</h3>
+      <Carousel />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+        }}
+      >
         <Button
-          className="btn-success"
+          style={{ width: "15rem" }}
+          size="lg"
+          variant="dark"
           onClick={() => {
             setCategoryId(null);
           }}
@@ -89,7 +49,9 @@ export default function Home() {
         {categories.map((category) => {
           return (
             <Button
-              className="btn-dark"
+              style={{ width: "15rem" }}
+              size="lg"
+              variant="dark"
               key={category.id}
               onClick={() => {
                 setCategoryId(category.id);
