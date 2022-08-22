a2cee45495cd0412e7a45d8928ff66f6066d7236
jake-burton29 <jacob.burton29@gmail.com>
2022-08-22 10:29:49 -0600

    working on utils functions

--- api/product_orders.js
+++ api/product_orders.js
@@ -1,10 +1,20 @@
 const product_orderRouter = require("express").Router();
 const prisma = require("../db/prisma");
+const { requireUserOrGuest } = require("./utils");
 
 // POST to PRODUCT_ORDERS
-product_orderRouter.post("/", async (req, res, next) => {
+product_orderRouter.post("/", requireUserOrGuest, async (req, res, next) => {
   try {
     const { orderId, productId, quantity } = req.body;
+    const order = await prisma.orders.findUnique({
+      where: { id: orderId },
+    });
+    if (order.shopperId !== req.user.id || order.complete) {
+      res.status(401).send({
+        loggedIn: false,
+        message: "You don't have access to this order.",
+      });
+    }
     const newProductOrder = await prisma.product_orders.create({
       data: {
         productId,
@@ -24,6 +34,20 @@ product_orderRouter.patch("/:productOrderId", async (req, res, next) => {
   try {
     const productOrderId = +req.params.productOrderId;
     const { quantity } = req.body;
+    const product_order = await prisma.orders.findUnique({
+      where: { id: productOrderId },
+      include: { orders: true },
+    });
+    if (
+      product_order.orders.shopperId !== req.user.id ||
+      product_order.orders.complete
+    ) {
+      res.status(401).send({
+        loggedIn: false,
+        message: "You don't have access to this order.",
+      });
+    }
+
     const updatedProductOrder = await prisma.product_orders.update({
       data: {
         quantity,
@@ -41,6 +65,19 @@ product_orderRouter.patch("/:productOrderId", async (req, res, next) => {
 product_orderRouter.delete("/:productOrderId", async (req, res, next) => {
   try {
     const productOrderId = +req.params.productOrderId;
+    const product_order = await prisma.orders.findUnique({
+      where: { id: productOrderId },
+      include: { orders: true },
+    });
+    if (
+      product_order.orders.shopperId !== req.user.id ||
+      product_order.orders.complete
+    ) {
+      res.status(401).send({
+        loggedIn: false,
+        message: "You don't have access to this order.",
+      });
+    }
     const updatedProductOrder = await prisma.product_orders.delete({
       where: { id: productOrderId },
     });
--- api/utils.js
+++ api/utils.js
@@ -36,4 +36,26 @@ const requireAdmin = (req, res, next) => {
   next();
 };
 
-module.exports = { requireUser, requireAdmin };
+const requireUserOrGuest = (req, res, next) => {
+  try {
+    const token = req.signedCookies.token;
+    if (!token) {
+      req.user.id = null;
+      next();
+    }
+    const user = jwt.verify(token, JWT_SECRET);
+    delete user.password;
+    req.user = user;
+    if (user) {
+      next();
+    }
+  } catch (error) {
+    res.status(401).send({
+      loggedIn: false,
+      message: "You'll need to register for access to this function!",
+    });
+    return;
+  }
+};
+
+module.exports = { requireUser, requireAdmin, requireUserOrGuest };
